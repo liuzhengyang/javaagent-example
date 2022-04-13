bc923721ae7d276d97d8db7f8532a92cd800b2f6
liuzhengyang <liuzhengyang75@gmail.com>
2022-04-13 16:40:51 +0800

    bytebuddy

---
+++ src/main/java/com/lzy/javaagent/TestByteBuddyInstall.java
@@ -0,0 +1,16 @@
+package com.lzy.javaagent;
+
+import java.lang.instrument.Instrumentation;
+
+import net.bytebuddy.agent.ByteBuddyAgent;
+
+/**
+ * @author liuzhengyang
+ * 2022/4/13
+ */
+public class TestByteBuddyInstall {
+    public static void main(String[] args) {
+        Instrumentation install = ByteBuddyAgent.install();
+//        install.addTransformer();
+    }
+}
